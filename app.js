ee329d992bc92ac4806ba7424e62a07cb592cee7
Cyrus Roshan <CyrusRoshan@users.noreply.github.com>
2016-06-10 11:30:55 -0400

    Fix making too many adjuster elements, transisitioning styles that don't need to be transisitoned.

--- app.js
+++ app.js
@@ -55,14 +55,43 @@
 
   let resizingCSS = document.createElement('style');
   document.head.appendChild(resizingCSS);
+
   let textResizer = null;
 
-  let updateElement = function(){
-
+  function onLoad() {
     textResizer = document.createElement('div');
-    textResizer.className = 'example-font-size-resizer';
-    resizingCSS.innerHTML = '';
     document.body.appendChild(textResizer)
+    textResizer.className = 'eager-apps-text-enlarger-app';
+
+    function createA(name) {
+      createdA = document.createElement('div');
+      createdA.innerText = 'A';
+      createdA.onclick = () => {resizeText(viewSize, name)};
+      return createdA;
+    }
+
+    Object.keys(sizeToRatio).forEach(size => {
+      textResizer.appendChild(createA(size));
+    })
+
+    update();
+  }
+
+  function resizeText(oldSize, newSize) {
+    let tagCSS = ''
+    if (newSize != 'original') {
+      let sizeMultiplier = sizeToRatio[newSize].actualSize / sizeToRatio[oldSize].actualSize;
+      htmlTags.forEach(tag => {
+        let firstElement = document.querySelector(tag);
+        if (firstElement) {
+          let newFontSize = parseFloat(window.getComputedStyle(document.querySelector(tag), null).fontSize) * sizeMultiplier;
+          tagCSS += `${tag} {
+            font-size: ${newFontSize}px !important;
+            line-height: 1.6 !important;
+          }\n`
+        }
+      })
+    }
 
     let verticalDirection, horizontalDirection;
     switch (options.corner) {
@@ -85,107 +114,77 @@
         break;
     };
 
-    function createA(name) {
-      createdA = document.createElement('div');
-      createdA.innerText = 'A';
-      createdA.onclick = () => {resizeText(viewSize, name)};
-      return createdA;
-    }
-
-    Object.keys(sizeToRatio).forEach(size => {
-      textResizer.appendChild(createA(size));
-    })
-
-    resizeText('original', viewSize || 'original');
-
-    function resizeText(oldSize, newSize) {
-      let tagCSS = ''
-      if (newSize != 'original') {
-        let sizeMultiplier = sizeToRatio[newSize].actualSize / sizeToRatio[oldSize].actualSize;
-        htmlTags.forEach(tag => {
-          let firstElement = document.querySelector(tag);
-          if (firstElement) {
-            let newFontSize = parseFloat(window.getComputedStyle(document.querySelector(tag), null).fontSize) * sizeMultiplier;
-            tagCSS += `${tag} {
-              font-size: ${newFontSize}px !important;
-              line-height: 1.6 !important;
-            }\n`
-          }
-        })
+    let textResizerCSS = (`
+      .${textResizer.className} {
+        ${verticalDirection}: 10px !important;
+        ${horizontalDirection}: 10px !important;
+        display: inline-block !important;
+        padding: 5px !important;
+        z-index: 2147483647 !important;
+        -moz-user-select: none !important;
+        -khtml-user-select: none !important;
+        -webkit-user-select: none !important;
+        -o-user-select: none !important;
+        border-radius: ${options.borderRadius}px !important;
+        background-color: ${options.backgroundColor} !important;
+        opacity: ${options.seeThrough ? 0.5 : 1} !important;
+        position: fixed !important;
+        -webkit-transition: opacity 500ms !important;
+        -moz-transition: opacity 500ms !important;
+        -o-transition: opacity 500ms !important;
+        transition: opacity 500ms !important;
       }
 
-      let textResizerCSS = (`
-        .${textResizer.className} {
-          ${verticalDirection}: 10px !important;
-          ${horizontalDirection}: 10px !important;
-          display: inline-block !important;
-          padding: 5px !important;
-          z-index: 2147483647 !important;
-          -moz-user-select: none !important;
-          -khtml-user-select: none !important;
-          -webkit-user-select: none !important;
-          -o-user-select: none !important;
-          border-radius: ${options.borderRadius}px !important;
-          background-color: ${options.backgroundColor} !important;
-          opacity: ${options.seeThrough ? 0.5 : 1} !important;
-          position: fixed !important;
-          -webkit-transition-duration: 500ms !important;
-          -moz-transition-duration: 500ms !important;
-          -o-transition-duration: 500ms !important;
-          transition-duration: 500ms !important;
-        }
+      .${textResizer.className}:hover {
+        opacity: ${options.seeThrough ? 0.8 : 1} !important;
+      }
 
-        .${textResizer.className}:hover {
-          opacity: ${options.seeThrough ? 0.8 : 1} !important;
-        }
+      .${textResizer.className} div {
+        float: left !important;
+        display: block !important;
+        margin: 0 !important;
+        font-weight: 200 !important;
+        font-family: arial, helvetica, verdana !important;
+        background-color: transparent !important;
+        cursor: pointer !important;
+        padding: ${options.fontSize / 5}px !important;
+        color: ${options.textColor} !important;
+        opacity: 0.5 !important;
+        -webkit-transition: opacity 500ms !important;
+        -moz-transition: opacity 500ms !important;
+        -o-transition: opacity 500ms !important;
+        transition: opacity 500ms !important;
+      }
+      .${textResizer.className} div:hover {
+        opacity: 0.8!important;
+      }
+      .${textResizer.className} div:nth-child(${Object.keys(sizeToRatio).indexOf(newSize) + 1}) {
+        opacity: 0.9 !important;
+      }
+      .${textResizer.className} div:nth-child(${Object.keys(sizeToRatio).indexOf(newSize) + 1}):hover {
+        opacity: 1 !important;
+      }
+    `);
 
-        .${textResizer.className} div {
-          float: left !important;
-          display: block !important;
-          margin: 0 !important;
-          font-weight: 200 !important;
-          font-family: arial, helvetica, verdana !important;
-          background-color: transparent !important;
-          cursor: pointer !important;
-          padding: ${options.fontSize / 5}px !important;
-          color: ${options.textColor} !important;
-          opacity: 0.5 !important;
-          -webkit-transition-duration: 500ms !important;
-          -moz-transition-duration: 500ms !important;
-          -o-transition-duration: 500ms !important;
-          transition-duration: 500ms !important;
-        }
-        .${textResizer.className} div:hover {
-          opacity: 0.8!important;
-        }
-        .${textResizer.className} div:nth-child(${Object.keys(sizeToRatio).indexOf(newSize) + 1}) {
-          opacity: 0.9 !important;
-        }
-        .${textResizer.className} div:nth-child(${Object.keys(sizeToRatio).indexOf(newSize) + 1}):hover {
-          opacity: 1 !important;
-        }
-      `);
+    let aCSS = ''
+    Object.keys(sizeToRatio).forEach(size => {
+      aCSS += `.${textResizer.className} div:nth-child(${Object.keys(sizeToRatio).indexOf(size) + 1}) {
+        line-height: ${options.fontSize * sizeToRatio[Object.keys(sizeToRatio).slice(-1)[0]].shownSize}px !important;
+        font-size: ${options.fontSize * sizeToRatio[size].shownSize}px !important;
+      }\n`
+    })
 
-      let aCSS = ''
-      Object.keys(sizeToRatio).forEach(size => {
-        aCSS += `.${textResizer.className} div:nth-child(${Object.keys(sizeToRatio).indexOf(size) + 1}) {
-          line-height: ${options.fontSize * sizeToRatio[Object.keys(sizeToRatio).slice(-1)[0]].shownSize}px !important;
-          font-size: ${options.fontSize * sizeToRatio[size].shownSize}px !important;
-        }\n`
-      })
+    // Save scroll ratio to be able to return user to the same position on the page after we've finished resizing the text
+    let scrollRatio = window.scrollY / document.documentElement.scrollHeight;
 
-      // Save scroll ratio to be able to return user to the same position on the page after we've finished resizing the text
-      let scrollRatio = window.scrollY / document.documentElement.scrollHeight;
+    resizingCSS.innerHTML = tagCSS + textResizerCSS + aCSS;
+    viewSize = newSize; // For browsers that can't set localStorage variables
+    try {
+      localStorage.EagerTextViewSize = newSize;
+    } catch (e) {}
 
-      resizingCSS.innerHTML = tagCSS + textResizerCSS + aCSS;
-      viewSize = newSize; // For browsers that can't set localStorage variables
-      try {
-        localStorage.EagerTextViewSize = newSize;
-      } catch (e) {}
-
-      // Return user to approximate previous scroll position
-      window.scroll(window.scrollX, scrollRatio * document.documentElement.scrollHeight);
-    }
+    // Return user to approximate previous scroll position
+    window.scroll(window.scrollX, scrollRatio * document.documentElement.scrollHeight);
   }
 
 
@@ -195,7 +194,7 @@
   //*********************
 
   let update = function(){
-    updateElement();
+    resizeText('original', viewSize || 'original');
   }
 
   let setOptions = function(opts){
@@ -206,9 +205,9 @@
   // Since we're adding an element to the body, we need to wait until the DOM is
   // ready before inserting our widget.
   if (document.readyState == 'loading')
-    document.addEventListener('DOMContentLoaded', update);
+    document.addEventListener('DOMContentLoaded', onLoad);
   else
-    update();
+    onLoad();
 
   // This is used by the preview to enable live updating of the app while previewing.
   // See the preview.handlers section of the install.json file to see where it's used.
--- dist/app.js
+++ dist/app.js
@@ -29,14 +29,45 @@
 
   var resizingCSS = document.createElement('style');
   document.head.appendChild(resizingCSS);
+
   var textResizer = null;
 
-  var updateElement = function updateElement() {
-
+  function onLoad() {
     textResizer = document.createElement('div');
-    textResizer.className = 'example-font-size-resizer';
-    resizingCSS.innerHTML = '';
     document.body.appendChild(textResizer);
+    textResizer.className = 'eager-apps-text-enlarger-app';
+
+    function createA(name) {
+      createdA = document.createElement('div');
+      createdA.innerText = 'A';
+      createdA.onclick = function () {
+        resizeText(viewSize, name);
+      };
+      return createdA;
+    }
+
+    Object.keys(sizeToRatio).forEach(function (size) {
+      textResizer.appendChild(createA(size));
+    });
+
+    update();
+  }
+
+  function resizeText(oldSize, newSize) {
+
+    var tagCSS = '';
+    if (newSize != 'original') {
+      (function () {
+        var sizeMultiplier = sizeToRatio[newSize].actualSize / sizeToRatio[oldSize].actualSize;
+        htmlTags.forEach(function (tag) {
+          var firstElement = document.querySelector(tag);
+          if (firstElement) {
+            var newFontSize = parseFloat(window.getComputedStyle(document.querySelector(tag), null).fontSize) * sizeMultiplier;
+            tagCSS += tag + ' {\n            font-size: ' + newFontSize + 'px !important;\n            line-height: 1.6 !important;\n          }\n';
+          }
+        });
+      })();
+    }
 
     var verticalDirection = void 0,
         horizontalDirection = void 0;
@@ -60,63 +91,33 @@
         break;
     };
 
-    function createA(name) {
-      createdA = document.createElement('div');
-      createdA.innerText = 'A';
-      createdA.onclick = function () {
-        resizeText(viewSize, name);
-      };
-      return createdA;
-    }
+    var textResizerCSS = '\n      .' + textResizer.className + ' {\n        ' + verticalDirection + ': 10px !important;\n        ' + horizontalDirection + ': 10px !important;\n        display: inline-block !important;\n        padding: 5px !important;\n        z-index: 2147483647 !important;\n        -moz-user-select: none !important;\n        -khtml-user-select: none !important;\n        -webkit-user-select: none !important;\n        -o-user-select: none !important;\n        border-radius: ' + options.borderRadius + 'px !important;\n        background-color: ' + options.backgroundColor + ' !important;\n        opacity: ' + (options.seeThrough ? 0.5 : 1) + ' !important;\n        position: fixed !important;\n        -webkit-transition: opacity 500ms !important;\n        -moz-transition: opacity 500ms !important;\n        -o-transition: opacity 500ms !important;\n        transition: opacity 500ms !important;\n      }\n\n      .' + textResizer.className + ':hover {\n        opacity: ' + (options.seeThrough ? 0.8 : 1) + ' !important;\n      }\n\n      .' + textResizer.className + ' div {\n        float: left !important;\n        display: block !important;\n        margin: 0 !important;\n        font-weight: 200 !important;\n        font-family: arial, helvetica, verdana !important;\n        background-color: transparent !important;\n        cursor: pointer !important;\n        padding: ' + options.fontSize / 5 + 'px !important;\n        color: ' + options.textColor + ' !important;\n        opacity: 0.5 !important;\n        -webkit-transition: opacity 500ms !important;\n        -moz-transition: opacity 500ms !important;\n        -o-transition: opacity 500ms !important;\n        transition: opacity 500ms !important;\n      }\n      .' + textResizer.className + ' div:hover {\n        opacity: 0.8!important;\n      }\n      .' + textResizer.className + ' div:nth-child(' + (Object.keys(sizeToRatio).indexOf(newSize) + 1) + ') {\n        opacity: 0.9 !important;\n      }\n      .' + textResizer.className + ' div:nth-child(' + (Object.keys(sizeToRatio).indexOf(newSize) + 1) + '):hover {\n        opacity: 1 !important;\n      }\n    ';
 
+    var aCSS = '';
     Object.keys(sizeToRatio).forEach(function (size) {
-      textResizer.appendChild(createA(size));
+      aCSS += '.' + textResizer.className + ' div:nth-child(' + (Object.keys(sizeToRatio).indexOf(size) + 1) + ') {\n        line-height: ' + options.fontSize * sizeToRatio[Object.keys(sizeToRatio).slice(-1)[0]].shownSize + 'px !important;\n        font-size: ' + options.fontSize * sizeToRatio[size].shownSize + 'px !important;\n      }\n';
     });
 
-    resizeText('original', viewSize || 'original');
+    // Save scroll ratio to be able to return user to the same position on the page after we've finished resizing the text
+    var scrollRatio = window.scrollY / document.documentElement.scrollHeight;
 
-    function resizeText(oldSize, newSize) {
-      var tagCSS = '';
-      if (newSize != 'original') {
-        (function () {
-          var sizeMultiplier = sizeToRatio[newSize].actualSize / sizeToRatio[oldSize].actualSize;
-          htmlTags.forEach(function (tag) {
-            var firstElement = document.querySelector(tag);
-            if (firstElement) {
-              var newFontSize = parseFloat(window.getComputedStyle(document.querySelector(tag), null).fontSize) * sizeMultiplier;
-              tagCSS += tag + ' {\n              font-size: ' + newFontSize + 'px !important;\n              line-height: 1.6 !important;\n            }\n';
-            }
-          });
-        })();
-      }
+    resizingCSS.innerHTML = tagCSS + textResizerCSS + aCSS;
+    viewSize = newSize; // For browsers that can't set localStorage variables
+    try {
+      localStorage.EagerTextViewSize = newSize;
+    } catch (e) {}
 
-      var textResizerCSS = '\n        .' + textResizer.className + ' {\n          ' + verticalDirection + ': 10px !important;\n          ' + horizontalDirection + ': 10px !important;\n          display: inline-block !important;\n          padding: 5px !important;\n          z-index: 2147483647 !important;\n          -moz-user-select: none !important;\n          -khtml-user-select: none !important;\n          -webkit-user-select: none !important;\n          -o-user-select: none !important;\n          border-radius: ' + options.borderRadius + 'px !important;\n          background-color: ' + options.backgroundColor + ' !important;\n          opacity: ' + (options.seeThrough ? 0.5 : 1) + ' !important;\n          position: fixed !important;\n          -webkit-transition-duration: 500ms !important;\n          -moz-transition-duration: 500ms !important;\n          -o-transition-duration: 500ms !important;\n          transition-duration: 500ms !important;\n        }\n\n        .' + textResizer.className + ':hover {\n          opacity: ' + (options.seeThrough ? 0.8 : 1) + ' !important;\n        }\n\n        .' + textResizer.className + ' div {\n          float: left !important;\n          display: block !important;\n          margin: 0 !important;\n          font-weight: 200 !important;\n          font-family: arial, helvetica, verdana !important;\n          background-color: transparent !important;\n          cursor: pointer !important;\n          padding: ' + options.fontSize / 5 + 'px !important;\n          color: ' + options.textColor + ' !important;\n          opacity: 0.5 !important;\n          -webkit-transition-duration: 500ms !important;\n          -moz-transition-duration: 500ms !important;\n          -o-transition-duration: 500ms !important;\n          transition-duration: 500ms !important;\n        }\n        .' + textResizer.className + ' div:hover {\n          opacity: 0.8!important;\n        }\n        .' + textResizer.className + ' div:nth-child(' + (Object.keys(sizeToRatio).indexOf(newSize) + 1) + ') {\n          opacity: 0.9 !important;\n        }\n        .' + textResizer.className + ' div:nth-child(' + (Object.keys(sizeToRatio).indexOf(newSize) + 1) + '):hover {\n          opacity: 1 !important;\n        }\n      ';
-
-      var aCSS = '';
-      Object.keys(sizeToRatio).forEach(function (size) {
-        aCSS += '.' + textResizer.className + ' div:nth-child(' + (Object.keys(sizeToRatio).indexOf(size) + 1) + ') {\n          line-height: ' + options.fontSize * sizeToRatio[Object.keys(sizeToRatio).slice(-1)[0]].shownSize + 'px !important;\n          font-size: ' + options.fontSize * sizeToRatio[size].shownSize + 'px !important;\n        }\n';
-      });
-
-      // Save scroll ratio to be able to return user to the same position on the page after we've finished resizing the text
-      var scrollRatio = window.scrollY / document.documentElement.scrollHeight;
-
-      resizingCSS.innerHTML = tagCSS + textResizerCSS + aCSS;
-      viewSize = newSize; // For browsers that can't set localStorage variables
-      try {
-        localStorage.EagerTextViewSize = newSize;
-      } catch (e) {}
-
-      // Return user to approximate previous scroll position
-      window.scroll(window.scrollX, scrollRatio * document.documentElement.scrollHeight);
-    }
-  };
+    // Return user to approximate previous scroll position
+    window.scroll(window.scrollX, scrollRatio * document.documentElement.scrollHeight);
+  }
 
   //*********************
   // Eager install code
   //*********************
 
   var update = function update() {
-    updateElement();
+    //updateElement();
+    resizeText('original', viewSize || 'original');
   };
 
   var setOptions = function setOptions(opts) {
@@ -126,7 +127,7 @@
 
   // Since we're adding an element to the body, we need to wait until the DOM is
   // ready before inserting our widget.
-  if (document.readyState == 'loading') document.addEventListener('DOMContentLoaded', update);else update();
+  if (document.readyState == 'loading') document.addEventListener('DOMContentLoaded', onLoad);else onLoad();
 
   // This is used by the preview to enable live updating of the app while previewing.
   // See the preview.handlers section of the install.json file to see where it's used.
